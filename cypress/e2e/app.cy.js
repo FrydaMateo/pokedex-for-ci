2630787f1562c5f4cadc9544084b9dd727e89ba3
FrydaMateo <frydasaezmateo@gmail.com>
2022-09-02 13:55:58 -0300

    Remove cypress

--- cypress/e2e/app.cy.js
+++
@@ -1,9 +0,0 @@
-/* global cy*/
-
-describe('Pokedex', function() {
-    it('frontpage can be opened', function() {
-        cy.visit('http://localhost:5000')
-        cy.contains('Pikachu')
-        cy.contains('Pokémon and pokémon character names are trademarks of Nintendo')
-    })
-})
